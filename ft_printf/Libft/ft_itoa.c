5c04d2706923123858ce784715b068dfb57a2a26
leote7-2 <161528927+leote7-2@users.noreply.github.com>
2024-05-07 17:53:36 +0100

    hahaha :)

---
+++ ft_printf/Libft/ft_itoa.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhenriq2 <jhenriq2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 16:52:46 by jhenriq2          #+#    #+#             */
+/*   Updated: 2024/05/03 12:51:12 by jhenriq2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned int	ft_count_digits(int n)
+{
+	unsigned int	c;
+
+	c = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		c += 1;
+	while (n != 0)
+	{
+		n = n / 10;
+		c++;
+	}
+	return (c);
+}
+/**@brief Integer to ascii
+ * @param param n Integer 
+ * @return The number in ascii
+*/
+
+char	*ft_itoa(int n)
+{
+	unsigned int		i;
+	char				*str;
+	unsigned int		n_digits;
+
+	n_digits = ft_count_digits(n);
+	str = (char *)malloc((n_digits + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	if (n < 0)
+	{
+		str[0] = '-';
+		i = -n;
+	}
+	else
+		i = n;
+	if (i == 0)
+		str[0] = '0';
+	str[n_digits] = 0;
+	while (i != 0)
+	{
+		str[n_digits - 1] = (i % 10) + '0';
+		i = i / 10;
+		n_digits--;
+	}
+	return (str);
+}
+/*int main()
+{
+	int test_cases[] = {0, 123, -456, 7890, -12345};
+	int num_cases = sizeof(test_cases) / sizeof(test_cases[0]);
+
+	printf("Testing ft_itoa function:\n");
+	for (int i = 0; i < num_cases; i++)
+	{
+		int num = test_cases[i];
+		char *result = ft_itoa(num);
+		if (result == NULL) 
+		{
+			printf("Memory allocation failed for test case %d\n", num);
+			continue;
+		}
+		printf("Number: %d, Result: %s\n", num, result);
+		free(result);
+	}
+
+	return (0);
+}*/
